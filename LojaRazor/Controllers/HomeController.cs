0076ddfcaf2cfdf3c4bb8b51c4968bdbddd7158b
Luis Mauro Penholato <luispenholato@gmail.com>
2016-02-13 18:35:29 -0200

    Contatos

--- LojaRazor/Controllers/HomeController.cs
+++ LojaRazor/Controllers/HomeController.cs
@@ -1,9 +1,10 @@
 ﻿using LojaRazor.DAO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using LojaRazor.Models;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace LojaRazor.Controllers
 {
@@ -27,5 +28,38 @@ namespace LojaRazor.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Contato(Contato objModelMail)
+        {
+            if (ModelState.IsValid)
+            {
+                string from = "xxxxxx@gmail.com"; //email gmail
+                using (MailMessage mail = new MailMessage(from, objModelMail.Email))
+                {
+                    mail.Subject = objModelMail.Nome;
+                    mail.Subject = objModelMail.Assunto;
+                    mail.Body = objModelMail.Mensagem;
+                    
+                    mail.IsBodyHtml = true;
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    NetworkCredential networkCredential = new NetworkCredential(from, "xxxxxx");//password gmail
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = networkCredential;
+                    smtp.Port = 587;
+                    smtp.Send(mail);
+                   
+                    return View("Contato", objModelMail);
+                }
+            }
+            else
+            {
+                return View();
+            }
+        }
+
     }
 }
+
