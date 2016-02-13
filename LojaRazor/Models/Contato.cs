32bd66f4d6e9dfa9d067107acbb4f810ee0b398d
Luis Mauro Penholato <luispenholato@gmail.com>
2016-02-13 18:38:42 -0200

    Contato

---
+++ LojaRazor/Models/Contato.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LojaRazor.Models
+{
+    public class Contato
+    {
+        [Required]
+        public string Nome { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Assunto { get; set; }
+
+        [Required]
+        public string Mensagem { get; set; }
+    }
+}
